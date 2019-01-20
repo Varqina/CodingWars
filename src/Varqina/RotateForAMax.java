ee259532c130e4f94d5d63f70f41a65eea93a71f
Varqina <flisu2@gmail.com>
2019-01-20 15:10:18 +0100

    addedtoverifyresolution

--- src/Varqina/RotateForAMax.java
+++ src/Varqina/RotateForAMax.java
@@ -45,4 +45,23 @@ public class RotateForAMax {
         }
         return max;
     }
+
+
+
+    //todo
+    // public static long maxRot (long n) {
+    //        String num = String.valueOf(n);
+    //
+    //        for (int i = 0; i < num.length() - 1; i++) {
+    //            num = num.substring(0, i) +
+    //                    num.substring(i + 1) +
+    //                    num.charAt(i);
+    //
+    //            if (Long.parseLong(num) > n) {
+    //                n = Long.parseLong(num);
+    //            }
+    //        }
+    //
+    //        return n;
+    //    }
 }
